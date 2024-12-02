c11247786dd1ba7a4c00b18b0200d346eb190fcb
Manjunatha <manjuamma99720@gmail.com>
2024-12-02 18:10:40 +0530

    module-and-component-architecture setup

---
+++ src/app/ingestion/ingestion-management/ingestion-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingestion-management',
+  templateUrl: './ingestion-management.component.html',
+  styleUrl: './ingestion-management.component.css'
+})
+export class IngestionManagementComponent {
+
+}
